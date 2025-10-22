362271e191c4e65af8690918b053d3b8c0eb5ccb
Rakesh Nair <crucifyrakesh@gmail.com>
2025-10-22 19:44:02 +0530

    Handle Runtime Exceptions

--- src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -12,6 +12,14 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleRuntimeException(Exception exception, WebRequest request){
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(request.getDescription(false)
+                , HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<ErrorResponseDto>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException customerAlreadyExistsException, WebRequest request){
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(request.getDescription(false)
