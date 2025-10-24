40138a2f46777aa1a1f70ffcb15a4b227e0bcbd0
Rakesh Nair <crucifyrakesh@gmail.com>
2025-10-24 20:50:26 +0530

    Spring openapi Documentation

--- src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts Microservice REST API Documentation",
+                version = "1.0",
+                description = "Accounts Service documentation",
+                contact = @Contact(
+                        name = "Rakesh Nair",
+                        email = "rakesh.nairs@eazybytes.com",
+                        url = "https://eazybytes.com"
+                ),
+                license = @License(
+                        name = "Eazybytes License",
+                        url = "https://eazybytes.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Accounts Service Documentation",
+                url = "https://eazybytes.com"
+        )
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
